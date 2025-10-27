f81727ce0d34cbc4d4db06f0e7bb80ce716dc1bc
Laura Engelhardt <laura.engelhardt@tum.de>
2025-10-27 14:53:24 +0100

    Rewrite muscle combo material using tensors

--- src/core/linalg/src/dense/4C_linalg_fixedsizematrix_tensor_derivatives.cpp
+++ src/core/linalg/src/dense/4C_linalg_fixedsizematrix_tensor_derivatives.cpp
@@ -7,6 +7,9 @@
 
 #include "4C_linalg_fixedsizematrix_tensor_derivatives.hpp"
 
+#include "4C_linalg_tensor.hpp"
+#include "4C_linalg_tensor_symmetric_einstein.hpp"
+
 
 
 FOUR_C_NAMESPACE_OPEN
@@ -56,6 +59,16 @@ void Core::LinAlg::FourTensorOperations::add_derivative_of_squared_tensor(
   C(5, 3) = scalar_this * C(5, 3) + scalar_squared_dx * 0.5 * X(1, 2);              // C1312
   C(5, 4) = scalar_this * C(5, 4) + scalar_squared_dx * 0.5 * X(0, 1);              // C1323
   C(5, 5) = scalar_this * C(5, 5) + scalar_squared_dx * 0.5 * (X(2, 2) + X(0, 0));  // C1313
+}
+
+Core::LinAlg::SymmetricTensor<double, 3, 3, 3, 3>
+Core::LinAlg::FourTensorOperations::derivative_of_inva_b_inva_product(
+    const Core::LinAlg::SymmetricTensor<double, 3, 3>& invA,
+    const Core::LinAlg::SymmetricTensor<double, 3, 3>& invABinvA)
+{
+  return -0.5 *
+         (einsum_sym<"ik", "jl">(invA, invABinvA) + einsum_sym<"il", "jk">(invA, invABinvA) +
+             einsum_sym<"jk", "il">(invA, invABinvA) + einsum_sym<"jl", "ik">(invA, invABinvA));
 }
 
 void Core::LinAlg::FourTensorOperations::add_derivative_of_inva_b_inva_product(double const& fac,
